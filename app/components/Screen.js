692825ce3ce56c3af9b9583d7a60bc341ea73781
lina-jamal <linahjamal89@gmail.com>
2020-09-13 03:10:35 +0300

    add Screen component

---
+++ app/components/Screen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StatusBar, StyleSheet } from "react-native";
+const Screen = ({ children }) => {
+  return <View style={styles.container}>{children}</View>;
+};
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+    paddingHorizontal: 15,
+    backgroundColor: "#f7f3f3",
+    flex: 1,
+  },
+});
+export default Screen;
