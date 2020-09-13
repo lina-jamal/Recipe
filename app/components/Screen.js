22b8e8868464f7837bc26f07a20eb36c7b6bb84e
lina-jamal <linahjamal89@gmail.com>
2020-09-13 16:45:09 +0300

    style screen , searcbar component

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { View, StatusBar, StyleSheet } from "react-native";
+import { View, StatusBar, StyleSheet, SafeAreaView } from "react-native";
 const Screen = ({ children }) => {
-  return <View style={styles.container}>{children}</View>;
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
 };
 const styles = StyleSheet.create({
   container: {
-    marginTop: StatusBar.currentHeight,
+    // marginTop: StatusBar.currentHeight,
     paddingHorizontal: 15,
     backgroundColor: "#f7f3f3",
     flex: 1,
+    alignItems: "center",
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
 export default Screen;
--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -3,17 +3,17 @@ import { View, TextInput, StyleSheet } from "react-native";
 const SearchBar = () => {
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Search here .." />
+      <TextInput style={styles.searchInput} placeholder="Search here .." />
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
+    width: "90%",
     height: 50,
+    // marginLeft: 20,
     backgroundColor: "white",
     borderRadius: 8,
-    justifyContent: "center",
   },
   searchInput: {
     width: "100%",
