6a14ac85fd7280ca3bebbbd1097e824de438ad6c
lina-jamal <linahjamal89@gmail.com>
2020-09-15 00:45:23 +0300

    make smallcard component

---
+++ app/components/smallCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Dimensions } from "react-native";
+const { width } = Dimensions.get("window");
+import BlockCard from "./BlockCard";
+const SmallCard = ({ item }) => {
+  return (
+    <BlockCard item={item} style={styles.container} imageStyle={styles.image} />
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    width: width / 2,
+    height: 200,
+    marginRight: 15,
+  },
+  image: {
+    height: 100,
+  },
+});
+export default SmallCard;
