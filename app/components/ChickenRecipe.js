6cc3a8f544a1d32f5948b2acbeb28aa4b5ee878b
lina-jamal <linahjamal89@gmail.com>
2020-09-16 21:27:27 +0300

    add chicken recipe component

---
+++ app/components/ChickenRecipe.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HorizotalList from "./HorizotalList";
+
+const ChickenRecipe = ({ data }) => {
+  return <HorizotalList title="CheckenRecipe" data={data} />;
+};
+
+export default ChickenRecipe;
