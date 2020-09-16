9bf5b47f39e7ea5c55f2012ab25f9c5ff5fc260f
lina-jamal <linahjamal89@gmail.com>
2020-09-16 21:26:28 +0300

    add horizotallist component

---
+++ app/components/HorizotalList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, FlatList, View } from "react-native";
+import Title from "./Title";
+import SmallCard from "./smallCard";
+const HorizotalList = ({ title, data }) => {
+  return (
+    <>
+      <Title style={styles.tittle} size={20}>
+        {title}
+      </Title>
+      <View style={styles.listStyle}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <SmallCard item={item} />}
+        />
+      </View>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  tittle: {
+    color: "red",
+    marginBottom: 10,
+  },
+  listStyle: {
+    marginVertical: 15,
+  },
+});
+export default HorizotalList;
