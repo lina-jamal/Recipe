85595a6b5234ed4fd495a5d4b7961dd4e15bace6
lina-jamal <linahjamal89@gmail.com>
2020-09-21 22:05:27 +0300

    make flat card component

---
+++ app/components/FlatCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import Title from "./Title";
+import SubTitle from "./SubTitle";
+
+const FlatCard = ({ item }) => {
+  const { image, title, deitLabel } = item;
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: image }} />
+      <View style={styles.contentContainer}>
+        <Title>{title}</Title>
+        <SubTitle> {deitLabel}</SubTitle>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 8,
+    overflow: "hidden",
+    marginBottom: 10,
+    height: 80,
+  },
+  image: {
+    flex: 0.35,
+    height: "100%",
+  },
+  contentContainer: {
+    flex: 0.65,
+    paddingHorizontal: 5,
+  },
+});
+export default FlatCard;
