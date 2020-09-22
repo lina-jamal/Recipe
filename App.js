40381876056093b196f068e2caf90ecab2cacac7
lina-jamal <linahjamal89@gmail.com>
2020-09-23 01:13:44 +0300

    make appNavigator and use navigation in app

--- App.js
+++ App.js
@@ -1,30 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
-import useResipe from "./app/hooks/useResipe";
-
-import Screen from "./app/components/common/Screen";
-import SearchBar from "./app/components/SearchBar";
-import FeaturedResipe from "./app/components/FeaturedResipe";
-import ChickenRecipe from "./app/components/ChickenRecipe";
-import MeatRecipe from "./app/components/meatRecipe";
-// import ResipeList from "./app/components/list/ResipeList";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./app/components/navigation/AppNavigator";
 export default function App() {
-  const [loading, chikenData, meatData] = useResipe();
   return (
-    <Screen>
-      <SearchBar />
-      <FeaturedResipe
-        item={{
-          id: "hhhjkkll;;",
-          // title: "Creamy Lemon Parmesan Chicken",
-          // dietLabels: "low",
-          image:
-            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190313-creamy-lemon-parmesan-chicken-horizontal-1553026901.png?crop=0.668xw:1.00xh;0.184xw,0&resize=768:*",
-        }}
-      />
-      {/* <SmallCard /> */}
-      {loading ? <Text>Loding</Text> : <ChickenRecipe data={chikenData} />}
-      {loading ? <Text>Loding</Text> : <MeatRecipe data={meatData} />}
-    </Screen>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
---
+++ app/components/navigation/AppNavigator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
+import Home from "../screens/Home";
+import IngredientLines from "../screens/ingredientLines";
+import ResipeList from "../list/ResipeList";
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="IngredientLines" component={IngredientLines} />
+      <Stack.Screen name="ResipeList" component={ResipeList} />
+    </Stack.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50",
+  },
+});
+
+export default AppNavigator;
