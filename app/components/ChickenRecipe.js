a0a215ca6450c94397e09ad487bdf87bf15ecc93
lina-jamal <linahjamal89@gmail.com>
2020-09-27 02:03:51 +0300

    fix the titil

--- app/components/ChickenRecipe.js
+++ app/components/ChickenRecipe.js
@@ -2,7 +2,7 @@ import React from "react";
 import HorizotalList from "./list/HorizotalList";
 
 const ChickenRecipe = ({ data }) => {
-  return <HorizotalList title="CheckenRecipe" data={data} />;
+  return <HorizotalList title="Checken Recipe.." data={data} />;
 };
 
 export default ChickenRecipe;
--- app/components/meatRecipe.js
+++ app/components/meatRecipe.js
@@ -2,7 +2,7 @@ import React from "react";
 import VerticaList from "./list/VerticaList";
 
 const MeatRecipe = ({ data }) => {
-  return <VerticaList title="MeatRecipe" data={data} />;
+  return <VerticaList title="Meat Recipe.." data={data} />;
 };
 
 export default MeatRecipe;
